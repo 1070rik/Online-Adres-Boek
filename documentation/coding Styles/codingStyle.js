5061d705d42faee41c8b5e51f16c48e62e07c8fc
Ian Vink <Ian.Vink@outlook.com>
2017-12-07 16:18:18 +0100

    Added coding styles

---
+++ documentation/coding Styles/codingStyle.js
@@ -0,0 +1,29 @@
+function important() {
+    var x;
+    console.log("This is important.");
+    while (true) {
+        if (true) {
+            if (false) {
+                console.log("test");
+                x = "Xje";
+            } else if (true) {
+                console.log("joiewe");
+                x = "Ook Xje";
+            } else {
+                console.log("joiewe");
+                x = "Geen Xje";
+            }
+        }
+    }
+    console.log(x);
+}
+
+
+function iets( x, y, color) {
+    //Yes black...
+    var created = false;
+    if (color = "black") {
+        created = true;
+    }
+    return created;
+}
