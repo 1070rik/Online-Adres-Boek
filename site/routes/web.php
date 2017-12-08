3a82e8e92bb08474b40e4ce3917b41e9b3481180
Rik <rik@nijdeken.com>
2017-12-08 11:29:19 +0100

    Fixed documentation filenames

--- site/routes/web.php
+++ site/routes/web.php
@@ -18,8 +18,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+<<<<<<< HEAD
 Route::get('/createUser', 'userController@createUser')->name('createUser');
 
 Route::post('/addUser', 'userController@create')->name('addUserPost');
+=======
+>>>>>>> Added contact controller
 Route::get('/addContact', 'contactsController@index')->name('addContactGet');
 Route::post('/addContact', 'contactsController@addContact')->name('addContactPost');
