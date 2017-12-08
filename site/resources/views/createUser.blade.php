6c6087466ab7be8096edbe392db2695d0201a037
Eliteforever-laptop <eliteforever_nath@hotmail.com>
2017-12-08 11:06:53 +0100

    Now able to create users on page /createUser

---
+++ site/resources/views/createUser.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+@section('content')
+<form class="form-horizontal" method="POST" action="{{ route('addUserPost') }}">
+    {{ csrf_field() }}
+
+    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+        <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+        <div class="col-md-6">
+            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+
+            @if ($errors->has('email'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+            @endif
+        </div>
+    </div>
+
+	<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+        <label for="admin" class="col-md-4 control-label">Is admin</label>
+
+        <div class="col-md-6">
+        	<select  for="admin" class="form-control" name="admin" required>
+        		<option value="0">Nee</option>
+        		<option value="1">Ja</option>
+        	</select>
+        </div>
+    </div>
+
+	<div class="form-group">
+    	<div class="col-md-8 col-md-offset-4">
+	        <button type="submit" class="btn btn-primary">
+	            Login
+	        </button>
+	    </div>
+	</div>
+</form>
+@endsection
