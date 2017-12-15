7242749cb20f97c06908c0867345fe59e84c2d4a
Eliteforever-laptop <eliteforever_nath@hotmail.com>
2017-12-15 11:15:12 +0100

    Created artisan command "importTestData'. created sql file which inserts fake contacts and addresses.

---
+++ site/app/Console/Commands/importTestData.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace AdresBoek\Console\Commands;
+
+use Illuminate\Console\Command;
+Use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
+
+class importTestData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'importTestData';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+
+    private function splitSQL($file, $delimiter = ';')
+        {
+        set_time_limit(0);
+        $queries = array();
+
+        if (is_file($file) === true)
+        {
+            $file = fopen($file, 'r');
+
+            if (is_resource($file) === true)
+            {
+                $query = array();
+
+                while (feof($file) === false)
+                {
+                    $query[] = fgets($file);
+
+                    if (preg_match('~' . preg_quote($delimiter, '~') . '\s*$~iS', end($query)) === 1)
+                    {
+                        $query = trim(implode('', $query));
+
+                        array_push($queries, $query);
+
+                        while (ob_get_level() > 0)
+                        {
+                            ob_end_flush();
+                        }
+
+                        flush();
+                    }
+
+                    if (is_string($query) === true)
+                    {
+                        $query = array();
+                    }
+                }
+
+                fclose($file);
+            }
+        }
+
+        return $queries;
+    }
+
+    public function handle()
+    {
+        Artisan::call('migrate:fresh');
+
+        $sql = file_get_contents('public/sql/fakeData.sql');
+
+        $queries = $this->splitSQL('public/sql/fakeData.sql');
+
+        foreach ($queries as $query){
+            DB::insert($query);  
+        }
+
+        
+    }
+}
--- site/database/seeds/DatabaseSeeder.php
+++ site/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $faker = Faker\Factory::create('nl_NL');
+
+        for ($i = 0; $i < 200; $i++){
+        	$address = $faker->address;
+
+        	print_r($address);
+        }
     }
 }
