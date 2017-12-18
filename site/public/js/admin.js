00bc8c4e26b2ddbb4ab20b4859455c12bf52cfa7
Eliteforever-laptop <eliteforever_nath@hotmail.com>
2017-12-18 11:33:07 +0100

    Added admin pages routes. Resolved css bug in page /admin/contacts where the side bar would stop earlier than expected. Resolved javascript bug for page /admin/contacts.

--- site/public/js/admin.js
+++ site/public/js/admin.js
@@ -1,12 +1,17 @@
 function getData(contact, addres) {
-  contact.tussenvoegsel.length<1&&(contact.tussenvoegsel="N/A");
-  $('.naam').text(contact['voornaam'] + ' ' + contact['tussenvoegsel'] + ' ' + contact['achternaam']);
-  $('.voornaam').text(contact['voornaam']);
-  $('.tussenvoegsel').text(contact['tussenvoegsel']);
-  $('.achternaam').text(contact['achternaam']);
-  $('.telefoonnummer').text(contact['telefoonnummer']);
-  $('.adres').text(addres['straatnaam'] + ' ' + addres['huisnummer']);
-  $('.plaats').text(addres['plaats']);
-  $('.postcode').text(addres['postcode']);
-  $('.opmerking').text(contact['beschrijving']);
+    if (contact.tussenvoegsel != null){
+        contact.tussenvoegsel.length<1&&(contact.tussenvoegsel = "N/A");
+    } else {
+        contact.tussenvoegsel = "N/A";
+    }
+
+    $('.naam').text(contact['voornaam'] + ' ' + contact['tussenvoegsel'] + ' ' + contact['achternaam']);
+    $('.voornaam').text(contact['voornaam']);
+    $('.tussenvoegsel').text(contact['tussenvoegsel']);
+    $('.achternaam').text(contact['achternaam']);
+    $('.telefoonnummer').text(contact['telefoonnummer']);
+    $('.adres').text(addres['straatnaam'] + ' ' + addres['huisnummer']);
+    $('.plaats').text(addres['plaats']);
+    $('.postcode').text(addres['postcode']);
+    $('.opmerking').text(contact['beschrijving']);
 }
