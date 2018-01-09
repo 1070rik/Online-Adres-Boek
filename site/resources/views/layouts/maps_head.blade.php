eb7c980e34c172f897a3c572253992cfe15674c6
Rik <rik@nijdeken.com>
2018-01-09 11:02:29 +0100

    fixed css errors

--- site/resources/views/layouts/maps_head.blade.php
+++ site/resources/views/layouts/maps_head.blade.php
@@ -1,8 +1,8 @@
 <html>
     <head>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-        <!-- <link rel="stylesheet" href="{{ asset('css/app_maps.css') }}"> -->
-        <link rel="stylesheet" href="{{ asset('css/app_map_phone.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/app_maps.css') }}">
+        <!-- <link rel="stylesheet" href="{{ asset('css/app_map_phone.css') }}"> -->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.min.js" integrity="sha384-3ziFidFTgxJXHMDttyPJKDuTlmxJlwbSkojudK/CkRqKDOmeSbN6KLrGdrBQnT2n" crossorigin="anonymous"></script>
