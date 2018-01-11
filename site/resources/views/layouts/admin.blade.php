c4865307431f4b04bab43b7852049ee04c6f8959
Eliteforever-laptop <eliteforever_nath@hotmail.com>
2018-01-11 14:54:50 +0100

    Fixed home button on admin panels

--- site/resources/views/layouts/admin.blade.php
+++ site/resources/views/layouts/admin.blade.php
@@ -43,7 +43,7 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                       <li>
-                        <a href="#">
+                        <a href="{{ url('/') }}">
                           <div class="nav-item">
                             Home
                           </div>
